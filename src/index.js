9002c36669b45922a81080a546c445902af47245
Etienne Mustow <etienne.o.mustow@gmail.com>
2021-03-09 20:00:23 +0000

    site changed to react, sticky navbar on scroll, no content

---
+++ src/index.js
@@ -0,0 +1,291 @@
+import ReactDOM, { render } from "react-dom"
+import React, { Component, useState, useRef, createRef } from "react"
+import state from "./store"
+import "./styles.css"
+import ModalVideo from 'react-modal-video'
+import "../node_modules/modal-video/js/jquery-modal-video";
+import "../node_modules/emailjs-com"
+import emailjs from 'emailjs-com';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { create } from "object-path"
+
+require('dotenv').config()
+
+
+// import{ init } from 'emailjs-com';
+// init("user_2t5SOqUvP0K225EiHwW6p");
+
+
+
+
+class NavBar extends React.Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.myFunction, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.myFunction);
+  }
+
+  constructor(props) {
+    super(props);
+    this.navbar = createRef();
+    this.navbarWrapper = createRef()
+  }
+
+
+ myFunction = () => {
+  var navbar = this.navbar.current
+  var navbarWrapper = this.navbarWrapper.current
+  var sticky = navbarWrapper.offsetTop;
+  console.log(document.documentElement.scrollTop)
+  console.log(document.documentElement.scrollTop > sticky)
+  if (document.documentElement.scrollTop >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+  render() {
+    return (
+      <div className="absolute-wrapper" ref={this.navbarWrapper}>
+      <nav className="navbar navbar-expand-lg navbar-light" id="navbar" ref={this.navbar}>
+        <a className="navbar-brand" href="/">Etienne Mustow | Software Developer</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      </div>
+    )
+  }
+}
+
+class Contact extends Component {
+
+  render() {
+    return (
+      <>
+        <div className="contact-page">
+          <div className="boxed">
+            <div className="contact-text">
+              <div className="centered-text">
+                <h2 className="contact-heading underline">Contact</h2>
+                <br></br>
+                <div className="underline"><h2>Get in touch:</h2></div>
+                <div className=""><a className="" href={`mailto:etncodes@gmail.com`}><h2>etncodes@gmail.com</h2></a></div>
+                <a href="https://www.linkedin.com/in/etncodes"><img className="contact-icon" src={state.icons.linkedin}></img></a>
+                <br></br><br></br>
+                <ContactForm />
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </>)
+  }
+}
+
+
+function Carousel({ pictures_location, carousel_id }) {
+  const pictures = pictures_location
+  return (
+    <div id={carousel_id} className="carousel slide carousel-fade" data-ride="carousel">
+      <div className="carousel-inner">
+        {pictures.map((picture) => {
+          if (picture.action == 0) {
+            return (<div className="carousel-item active" key={picture.key + picture.action}>
+              <div className="crop">
+                <img className="d-block w-100" src={picture.image} alt="First slide"  ></img>
+              </div>
+            </div>)
+          } else {
+            return (<div className="carousel-item" key={picture.key + picture.action}>
+              <div className="crop">
+                <img className="d-block w-100" src={picture.image} alt="Second slide" ></img>
+              </div>
+            </div>)
+          }
+        }
+        )}
+
+      </div>
+      <a className="carousel-control-prev" href={'#' + carousel_id} role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href={'#' + carousel_id} role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+    </div>
+  )
+}
+
+
+
+function RenderArtCarousel({ index, action_function, image_class }) {
+  return (
+    <figure className={index.css + index.action} key={index.action_icon} onClick={(e) => {
+      e.stopPropagation()
+      { action_function() }
+    }}>
+      <div className="overlay">
+
+        <img className={image_class} src={index.image}></img>
+        <div className="text-header">{index.header}</div>
+        <div className="text-footer">{index.text}</div>
+      </div>
+
+    </figure>
+  )
+}
+
+function Heading({ text }) {
+  return (
+    <div className="heading">
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+
+
+class Home extends Component {
+
+  render() {
+    return (
+      <>
+
+        <div className="page">
+          <Heading text="Hey, I'm Etienne." />
+        </div>
+          <NavBar />
+        <div>
+          <br></br>        <br></br>
+          <br></br>
+          <br></br>
+          <div className="content">
+            <section id="about">
+              <h1>About</h1>
+              <h2>I'm a junior software developer with just over a year's experience working in start-ups and mid-size businesses.
+        I'm also a Computer Science student at City, University of London</h2>
+            </section>
+            <br></br>        <br></br>
+            <br></br>
+            <br></br>
+            <section id="projects">
+              <h1>Projects</h1>
+              <h2>Here are some things I've built</h2>
+            </section>
+            <br></br>        <br></br>
+            <br></br>
+            <br></br>
+            <section id="contact">
+              <Contact />
+            </section>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+function App() {
+
+
+  return (
+    <Router  >
+
+      <Switch >
+
+        <Route exact path="/" component={Home} />
+
+
+
+      </Switch>
+
+    </Router>
+
+  )
+}
+
+
+
+
+class ContactForm extends Component {
+
+  state = {
+    name: '',
+    message: '',
+    email: '',
+    sent: false,
+    buttonText: 'Send Message'
+  }
+
+
+  sendEmail = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      buttonText: '...sending'
+    })
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_USER_ID)
+      .then((result) => {
+        console.log(result.text);
+        this.setState({ sent: true }, this.resetForm())
+      }, (error) => {
+        console.log(error.text);
+        console.log("service id: " + process.env.REACT_APP_SERVICE_ID + "\n template id: " + process.env.REACT_APP_TEMPLATE_ID + "\n user_id: " + process.env.REACT_APP_USER_ID)
+      });
+
+  }
+
+  resetForm = () => {
+    this.setState({
+      name: '',
+      message: '',
+      email: '',
+      buttonText: 'Message Sent'
+    })
+  }
+
+  render() {
+
+    return (
+      <form className="contact-form" onSubmit={(e) => this.sendEmail(e)}>
+        <div>
+          <input onChange={e => this.setState({ name: e.target.value })} type="text" name="user_name" placeholder="Your name" required value={this.state.name} />
+        </div>
+        <div>
+          <input onChange={(e) => this.setState({ email: e.target.value })} type="email" name="user_email" placeholder="Your email" required value={this.state.email} />
+        </div>
+        <div>
+          <textarea onChange={e => this.setState({ message: e.target.value })} name="message" placeholder="Your message" value={this.state.message} required />
+        </div>
+        <button type="submit">{this.state.buttonText}</button>
+        {/* <input type="submit" value="Send" /> */}
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ src/store.js
@@ -0,0 +1,460 @@
+
+
+const state = {
+  
+  sections: 9,
+  pages: 8,
+  zoom: 75,
+  icons: {
+    linkedin: "linkedin_icon.png",
+    email: "email.png",
+  },
+  paragraphs: {
+    production_design: [
+      {
+      header: "TTRRUUCES - TTRRUUCES",
+      image: "/truuuces.png",
+      text1: "directed by Maxim Kelly",
+      text2: "production design by Jimmy Van Twest",
+      video: "380030498",
+      channel: "vimeo",
+      action: 0,
+      css: "gallery__item gallery__item--"
+    },
+    {
+      header: "Finn Foxell- Make It Count",
+      image: "/makeitcount.jpg",
+      text1:
+      "directed by Milo Blake",
+      text2: "production design by Jimmy Van Twest",
+      video: "450474177",
+      channel: "vimeo",
+      action: 1,
+      css: "gallery__item gallery__item--"
+    },
+    {
+      header: "Rachel Chinouriri - Darker Place",
+      image: "/darker_place.webp",
+      text1:
+      "directed by Milo Blake",
+      text2: "production design by Jimmy Van Twest",
+      video: "65xdvUfH4tM",
+      channel: "youtube",
+      action: 2,
+      css: "gallery__item gallery__item--"
+    }
+  ],
+  render_art: {
+    apartment: [
+    {
+    header: "80’s New York Apartment Interpretation. 2020.",
+    key: "apartment",
+    image: "/render_art/blonde.png",
+    text: "designed by me.",
+    action: 0,
+    action_icon: 0,
+    css: "gallery__item gallery__item--"
+  },
+  {
+    header: "80’s New York Apartment Interpretation. 2020.",
+    key: "apartment",
+    image: "/render_art/blonde2.png",
+    text: "designed by me.",
+    action: 1,
+    css: "gallery__item gallery__item--"
+  },
+  {
+    header: "80’s New York Apartment Interpretation. 2020.",
+    key: "apartment",
+    image: "/render_art/blonde3.png",
+    text: "designed by me.",
+    action: 2,
+    css: "gallery__item gallery__item--"
+  },
+  {
+    header: "80’s New York Apartment Interpretation. 2020.",
+    key: "apartment",
+    image: "/render_art/blonde4.png",
+    text: "designed by me.",
+    action: 3,
+    css: "gallery__item gallery__item--"
+  },
+  {
+    header: "80’s New York Apartment Interpretation. 2020.",
+    key: "apartment",
+    image: "/render_art/blonde5.png",
+    text: "designed by me.",
+    action: 4,
+    css: "gallery__item gallery__item--"
+  }],
+  gallery: [
+    {
+    header: "Gallery Experiment. 2019.",
+    key: "gallery",
+    image: "/render_art/gallery.JPG",
+    text: "designed by me.",
+    action: 0,
+    action_icon: 1,
+    css: "gallery__item gallery__item--"
+  },
+  {
+    header: "Gallery Experiment. 2019.",
+    key: "gallery",
+    image: "/render_art/gallery2.JPG",
+    text: "designed by me.",
+    action: 1,
+    css: "gallery__item gallery__item--"
+  },
+  {
+    header: "Gallery Experiment. 2019.",
+    key: "gallery",
+    image: "/render_art/gallery3.JPG",
+    text: "designed by me.",
+    action: 2,
+    css: "gallery__item gallery__item--"
+  }
+],
+smart_energy: [
+  {
+  header: "Smart Energy GB Commercial",
+  key: "smart_energy",
+  image: "/render_art/still.jpg",
+  text: "designed by Tim Gibson.",
+  action: 0,
+  action_icon: 2,
+  css: "gallery__item gallery__item--"
+},
+{
+  header: "Gallery Experiment. 2019.",
+  key: "smart_energy",
+  image: "/render_art/still2.jpg",
+  text: "designed by me.",
+  action: 1,
+  css: "gallery__item gallery__item--"
+},
+{
+  header: "Smart Energy GB Commercial",
+  key: "smart_energy",
+  image: "/render_art/still3.jpg",
+  text: "designed by Tim Gibson",
+  action: 2,
+  css: "gallery__item gallery__item--"
+}
+]},
+  art_assisting: [
+    {
+      css: "gallery__item gallery__item--",
+      header: "GCDS FW 21-22 Fashion Show",
+      image: "/gcds.png",
+      text:
+      "designed by Thomas Bird",
+      video: "BVZ4dk8xmSk",
+      channel: "youtube",
+      action: 0
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "The Streets, IDLES - None Of Us Are Getting Out Of This Life Alive",
+      image: "/thestreetsnoneofus.jpg",
+      text:
+      "designed by Bon Walsh",
+      video: "SIZ7HXooV20",
+      channel: "youtube",
+      action: 1
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "British Vogue x Dua Lipa - Feb 2021 Cover",
+      image: "/voguedualipa2.jpg",
+      text: "designed by Emma Roach",
+      video: "464147830",
+      channel: "vimeo",
+      action: 2,
+      gallery: [
+          {
+          header: "British Vogue x Dua Lipa - Feb 2021 Cover",
+          key: "vogue",
+          image: "/vogue/vogue7.jpg",
+          text: "designed by Emma Roach.",
+          action: 0,
+          action_icon: 0,
+          css: "gallery__item gallery__item--"
+        },
+        {
+          header: "British Vogue x Dua Lipa - Feb 2021 Cover",
+          key: "vogue",
+          image: "/vogue/vogue8.jpg",
+          text: "designed by Emma Roach.",
+          action: 1,
+          css: "gallery__item gallery__item--"
+        },
+        {
+          header: "British Vogue x Dua Lipa - Feb 2021 Cover",
+          key: "vogue",
+          image: "/vogue/bvogue14.png",
+          text: "designed by me.",
+          action: 2,
+          css: "gallery__item gallery__item--"
+        },
+        {
+          header: "British Vogue x Dua Lipa - Feb 2021 Cover",
+          key: "vogue",
+          image: "/vogue/vogue1.jpg",
+          text: "designed by Emma Roach",
+          action: 3,
+          css: "gallery__item gallery__item--"
+        },
+      ]
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Pa Salieu | Jimmy Fallon Live Performance",
+      image: "/pa_salieu.jpg",
+      text:
+      "designed by Sarah Asmail",
+      video: "eLdQI7KAYl8",
+      channel: "youtube",
+      action: 3
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Rudimental - Come Over",
+      image: "/rudimentalcomeover.jpg",
+      text: "designed by Louis Simonon",
+      video: "464147830",
+      channel: "vimeo",
+      action: 4
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Farfetch: Open Doors To A World of Fashion",
+      image: "/farfetch.jpg",
+      text: "designed by Emma Roach",
+      video: "qLPPGeTIuds",
+      channel: "youtube",
+      action: 5
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Tonight With Arlo Parks Trailer",
+      image: "/arlo_parks.png",
+      text:
+      "designed by Sarah Asmail",
+      video: "faEZDFlihPU",
+      channel: "youtube",
+      action: 6
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Flohio, Clara Amfo and Shaquille-Aaron Keith discuss womanhood, Blackness and artistry",
+      image: "/flohio.jpeg",
+      text:
+      "designed by Ibby Njoya",
+      video: "-7lGjE9zzOA",
+      channel: "youtube",
+      action: 7
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "A-COLD-WALL* AUTUMN/WINTER 2021",
+      image: "/a_cold_wall.jpg",
+      text:
+      "designed by Thomas Bird",
+      video: "AYI-_aYq0_U",
+      channel: "youtube",
+      action: 8
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "No Naughty List | Tesco Christmas Commercial",
+      image: "/tesco.jpg",
+      text:
+      "designed by Bon Walsh",
+      video: "8CfrpexaCwg",
+      channel: "youtube",
+      action: 9
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "MINI | We're Different, But Pretty Good Together",
+      image: "/mini.jpg",
+      text:
+      "designed by Nathan Parker",
+      video: "xHm6ZIvbG14",
+      channel: "youtube",
+      action: 10
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Terry's Chocolate Orange 'Deliciously Unsquare' Commercial",
+      image: "/terrys.jpg",
+      text:
+      "designed by Tim Gibson",
+      video: "f7xuOo2DSjc",
+      channel: "youtube",
+      action: 11
+    }, 
+    {
+      css: "gallery__item gallery__item--",
+      header: "Just Eat - We Got it Commercial",
+      image: "/just_eat.jpg",
+      text:
+      "designed by Tim Gibson",
+      video: "TAcQDD2UBOg",
+      channel: "youtube",
+      action: 12
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "BT Sport Unlimited",
+      image: "/bt_sport.png",
+      text:
+      "designed by Bon Walsh",
+      video: "464953943",
+      channel: "vimeo",
+      action: 13
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Holly Humberstone | James Corden Live Performance",
+      image: "/holly_humberstone.png",
+      text:
+      "Standby Art Direction by Jimmy Van Twest",
+      video: "https://youtu.be/i7wLQnnpxYo",
+      channel: "youtube",
+      action: 14
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Current Account Switch Service Commercial",
+      image: "/currentaccount.png",
+      text:
+      "designed by Benny Casey",
+      video: "vWSC1Z8iCyc",
+      channel: "youtube",
+      action: 15
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "River Island 'In a Bubble' Commercial",
+      image: "/inabubble.jpg",
+      text:
+      "designed by Thomas Bird",
+      video: "iKMkXp3Xs3s",
+      channel: "youtube",
+      action: 16
+    },
+
+    {
+      css: "gallery__item gallery__item--",
+      header: "Fredo - Money Talks Feat. Dave",
+      image: "/money_talks.png",
+      text:
+      "designed by Elena Muntoni",
+      video: "VFK2DoDaAUc",
+      channel: "youtube",
+      action: 17
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Nines - Clout",
+      image: "/nineclout.png",
+      text: "designed by Jakob Gierse",
+      video: "kgUV1MaD_M8",
+      channel: "youtube",
+      action: 18
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Jeshi - Mask On",
+      image: "/jeshimaskon.jpg",
+      text:
+      "designed by Liz El-Kadhi",
+      video: "-7LtIlQRwBg",
+      channel: "youtube",
+      action: 19
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Nines - Airplane Mode",
+      image: "/ninesairplanemode.jpg",
+      text:
+      "designed by Jakob Gierse",
+      video: "XPZgehksWo4",
+      channel: "youtube",
+      action: 20
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Boston Bun - Don't Wanna Dance",
+      image: "/bostonbun.jpg",
+      text:
+      "designed by Elena Muntoni",
+      video: "IogVoBaYmvA",
+      channel: "youtube",
+      action: 21
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Jones - Camera Flash",
+      image: "/jonescameraflash.jpg",
+      text:
+      "designed by Benny Casey",
+      video: "446441649",
+      channel: "vimeo",
+      action: 22
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Stand Atlantic - Jurassic Park",
+      image: "/standatlanticjurassicpark.jpg",
+      text: "designed by Benny Casey",
+      video: "440368058",
+      channel: "vimeo",
+      action: 23
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Aaron Taylor - Flowers",
+      image: "/aarontaylorflowers.jpg",
+      text:
+      "designed by Elena Muntoni",
+      video: "t0LemwCvZq8",
+      channel: "youtube",
+      action: 24
+    },
+
+    {
+      css: "gallery__item gallery__item--",
+      header: "London Grammar - Californian Soil",
+      image: "/londongrammar.png",
+      text: "designed by Jakob Gierse",
+      video: "469366986",
+      channel: "vimeo",
+      action: 25
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "SL - Little Bird",
+      image: "/sllittlebird.jpg",
+      text:
+      "designed by Jakob Gierse",
+      video: "ZJvDBltodUE",
+      channel: "youtube",
+      action: 26
+    },
+    {
+      css: "gallery__item gallery__item--",
+      header: "Adidas x Superstan Commercial",
+      image: "/adidassuperstan.jpg",
+      text: "designed by Benny Casey",
+      video: "419372557",
+      channel: "vimeo",
+      action: 27
+    },
+  ]
+},
+  
+}
+
+export default state
